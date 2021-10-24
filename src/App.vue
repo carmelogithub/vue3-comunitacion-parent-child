0913260bda9cdc30dc7532e38941bc8a0e1c6266
carmelo escribano <carmelo.escribano@gmail.com>
2021-10-24 20:56:04 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <child-component
+    @emit-fun1="funParent1"
+    @emit-fun2="funParent2($event)"
+    @emit-fun3="funParent3"
+  ></child-component>
+  <div>
+    <span>{{ dText }}</span>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import childComponentVue from "./components/childComponent.vue";
+export default {
+  name: "App",
+  components: {
+    childComponent: childComponentVue,
+  },
+  setup() {
+    const dText = ref("texto original");
+    // !!!!!!!!!!!!!!!!!!! begin
+    const funParent1 = () => {
+      dText.value = "text-changed-by-funParent1";
+    };
+    const funParent2 = (parameter) => {
+      dText.value = "text-changed-by-funParent2    " + parameter;
+    };
+    const funParent3 = () => {
+      dText.value = "text-changed-by-funParent3";
+    };
+    // !!!!!!!!!!!!!!!!!!! end
+    return {
+      dText,
+      funParent1,
+      funParent2,
+      funParent3,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 400px;
+  width: 300px;
+  background: green;
+}
+</style>
---
+++ src/components/childComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="child-container">
+    <button @click="fun1()">boton 1</button>
+    <button @click="fun2()">boton 2</button>
+    <button @click="$emit('emit-fun3')">boton 3</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "childComponent",
+  emits: ["emit-fun1", "emit-fun2", "emit-fun3"],
+  setup(props, { emit }) {
+    const fun1 = () => {
+      emit("emit-fun1");
+    };
+    const fun2 = () => {
+      emit("emit-fun2", "parameter-from-child");
+    };
+    return {
+      fun1,
+      fun2,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.child-container {
+  width: 100px;
+  height: 200px;
+  background: red;
+}
+</style>
